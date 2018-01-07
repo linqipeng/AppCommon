341d95b61ccae4bd29d87a0372a70585d54eb9d3
Keep <L437943145@gmail.com>
2018-01-07 20:22:56 +0800

    nothing

--- src/main/java/xyz/no21/appcommon/base/BaseFragment.java
+++ src/main/java/xyz/no21/appcommon/base/BaseFragment.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 
-import java.util.ArrayList;
-
 import xyz.no21.appcommon.RetrofitUtils;
 
 
